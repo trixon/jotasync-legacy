413d7583029b3a96afb1339dcb34c90e0b59ba36
Patrik Karlsson <patrik@trixon.se>
2016-01-08 11:44:47 +0100

    Hide popup menu with escape.

--- src/main/java/se/trixon/jota/client/ui/TabHolder.java
+++ src/main/java/se/trixon/jota/client/ui/TabHolder.java
@@ -285,6 +285,20 @@ public class TabHolder extends JTabbedPane implements ConnectionListener, Server
                 if (e == null || e.getButton() == MouseEvent.BUTTON1) {
                     Component component = ((TabListener) getSelectedComponent()).getMenuButton();
                     JPopupMenu popupMenu = MainFrame.getPopupMenu();
+                    InputMap inputMap = popupMenu.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+                    ActionMap actionMap = popupMenu.getActionMap();
+                    Action action = new AbstractAction("HideMenu") {
+
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            popupMenu.setVisible(false);
+                        }
+                    };
+
+                    String key = "HideMenu";
+                    actionMap.put(key, action);
+                    KeyStroke keyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0);
+                    inputMap.put(keyStroke, key);
 
                     if (popupMenu.isVisible()) {
                         popupMenu.setVisible(false);
